f12726d02d0af7ab9a1aa76948014ee60e0e2549
xala3pa <salazar3pa@gmail.com>
2017-03-04 13:00:18 +0100

    test should no return any trip when user are no friends

--- java/trip-service-kata/src/test/java/org/craftedsw/tripservicekata/trip/TripServiceTest.java
+++ java/trip-service-kata/src/test/java/org/craftedsw/tripservicekata/trip/TripServiceTest.java
@@ -4,10 +4,18 @@ import org.craftedsw.tripservicekata.exception.UserNotLoggedInException;
 import org.craftedsw.tripservicekata.user.User;
 import org.junit.Test;
 
+import java.util.List;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.is;
+
 public class TripServiceTest {
 
     private static final User GUEST = null;
     private static final User UNUSED_USER = null;
+    private static final User REGISTER_USER = new User();
+    private static final User ANOTHER_USER = new User();
+    private static final Trip TO_SANFRANCISCO = new Trip();
     private User loggedInUser;
 
     @Test(expected = UserNotLoggedInException.class)
@@ -18,6 +26,23 @@ public class TripServiceTest {
 
         tripService.getTripsByUser(UNUSED_USER);
     }
+
+    @Test
+    public void should_no_return_any_trips_when_users_are_not_friends() {
+        TripService tripService = new TestableTripService();
+
+        loggedInUser = REGISTER_USER;
+
+        User friend = new User();
+        friend.addFriend(ANOTHER_USER);
+        friend.addTrip(TO_SANFRANCISCO);
+
+        List<Trip> friendTrips = tripService.getTripsByUser(friend);
+
+        assertThat(friendTrips.size(), is(0));
+    }
+
+
 
     private class TestableTripService extends TripService {
         @Override
