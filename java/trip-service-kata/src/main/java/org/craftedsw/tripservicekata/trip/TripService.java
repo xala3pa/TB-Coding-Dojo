ee523683a55b36f344b45d6493ed9d2218399927
xala3pa <salazar3pa@gmail.com>
2017-03-05 19:43:41 +0100

    Use ternary operator

--- java/trip-service-kata/src/main/java/org/craftedsw/tripservicekata/trip/TripService.java
+++ java/trip-service-kata/src/main/java/org/craftedsw/tripservicekata/trip/TripService.java
@@ -10,16 +10,11 @@ import org.craftedsw.tripservicekata.user.UserSession;
 public class TripService {
 
     public List<Trip> getTripsByUser(User user) throws UserNotLoggedInException {
-        User loggedUser = getLoggedUser();
-        if (loggedUser == null) {
+        if (getLoggedUser() == null) {
             throw new UserNotLoggedInException();
         }
 
-        List<Trip> tripList = new ArrayList<>();
-        if (user.isFriendWith(loggedUser)) {
-            tripList = getTripsBy(user);
-        }
-        return tripList;
+        return user.isFriendWith(getLoggedUser()) ? getTripsBy(user) : new ArrayList<>();
     }
 
     protected List<Trip> getTripsBy(User user) {
